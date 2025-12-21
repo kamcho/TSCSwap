4327ec0b8f11e4994c85e736b8d297ce9daa0df4
kamcho <njokevin9@gmail.com>
2025-12-21 17:25:21 +0300

    ai

--- TSCSwap/settings.py
+++ TSCSwap/settings.py
@@ -127,15 +127,33 @@ TEMPLATES = [
 WSGI_APPLICATION = 'TSCSwap.wsgi.application'
 
 
-# Database
+# Database Configuration
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.getenv('ENVIRONMENT') == 'PROD':
+    # Production Database (MySQL)
+    DATABASES = {
+        'default': {
+            'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.mysql'),
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', '3306'),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+                'charset': 'utf8mb4',
+            }
+        }
     }
-}
+else:
+    # Development Database (SQLite)
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, os.getenv('DB_NAME_DEV', 'db.sqlite3')),
+        }
+    }
 
 
 # Password validation
